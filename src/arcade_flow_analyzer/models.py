74a67920038f82cb628d41d52972538460af4c18
Kevin Stallone <kevin@stallone.io>
2025-10-13 22:17:19 -0700

    Basic pydantic models. More to be added if needed. Most fields assumed optional unless otherwise specified

---
+++ src/arcade_flow_analyzer/models.py
@@ -0,0 +1,65 @@
+"""
+Arcade Flow pydantic models
+
+Only verifies the data used in project analysis.
+
+Most fields are optional as they are not always present in the data.
+
+TODO: Add more fields as project expands.
+Current fields are thought to be enough for basic analysis
+"""
+
+from typing import List, Optional
+from pydantic import BaseModel, Field
+
+
+class Timestamp(BaseModel):
+    seconds: int = Field(alias="_seconds")
+    nanoseconds: int = Field(alias="_nanoseconds")
+
+
+class CapturedEvent(BaseModel):
+    type: str
+    timeMs: Optional[int] = None
+    startTimeMs: Optional[int] = None
+    endTimeMs: Optional[int] = None
+    clickId: Optional[str] = None
+    frameX: Optional[float] = None
+    frameY: Optional[float] = None
+
+
+class ClickContext(BaseModel):
+    text: Optional[str] = None
+    elementType: Optional[str] = None
+
+
+class PageContext(BaseModel):
+    url: Optional[str] = None
+    title: Optional[str] = None
+
+
+class Step(BaseModel):
+    id: Optional[str] = None
+    type: str
+    title: Optional[str] = None
+    subtitle: Optional[str] = None
+    clickContext: Optional[ClickContext] = None
+    pageContext: Optional[PageContext] = None
+
+
+class FlowData(BaseModel):
+    name: str
+    description: str
+    createdBy: str
+    teamId: str
+    useCase: str
+    hasUsedAI: bool
+    schemaVersion: str
+    status: int
+    created: Timestamp
+    capturedEvents: List[CapturedEvent]
+    steps: List[Step]
+
+    class Config:
+        populate_by_name = True
+        validate_assignment = True
